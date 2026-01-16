22a546e614213c33a695e0da11e66dc3ec980e27
FARHANFADHILA <farhanfd.112003@gmail.com>
2026-01-16 17:18:25 +0700

    ⚙️refactor: prettier structure

--- src/services/data-master/universitas.service.ts
+++ src/services/data-master/universitas.service.ts
@@ -1,7 +1,7 @@
-import { injectable } from "tsyringe";
-import { Prisma } from "@generated/prisma/client";
-import { UniversitasRepositories } from "@repositories/data-master/universitas.repositories";
-import { HTTPError } from "@utils/http-error";
+import { Prisma } from '@generated/prisma/client';
+import { UniversitasRepositories } from '@repositories/data-master/universitas.repositories';
+import { HTTPError } from '@utils/http-error';
+import { injectable } from 'tsyringe';
 
 @injectable()
 export class UniversitasService {
@@ -16,8 +16,8 @@ export class UniversitasService {
       return await this.repoUniversitas.createUniversitas(data);
     } catch (error) {
       if (error instanceof Prisma.PrismaClientKnownRequestError) {
-        if (error.code === "P2002") {
-          throw new HTTPError(409, "Data niversitas sudah ada (Duplikat).");
+        if (error.code === 'P2002') {
+          throw new HTTPError(409, 'Data niversitas sudah ada (Duplikat).');
         }
       }
       throw error;
@@ -29,8 +29,8 @@ export class UniversitasService {
       return await this.repoUniversitas.updateUniversitas(id, data);
     } catch (error) {
       if (error instanceof Prisma.PrismaClientKnownRequestError) {
-        if (error.code === "P2025") {
-          throw new HTTPError(404, "Data universitas tidak ditemukan.");
+        if (error.code === 'P2025') {
+          throw new HTTPError(404, 'Data universitas tidak ditemukan.');
         }
       }
       throw error;
@@ -42,8 +42,8 @@ export class UniversitasService {
       return await this.repoUniversitas.deleteUniversitas(id);
     } catch (error) {
       if (error instanceof Prisma.PrismaClientKnownRequestError) {
-        if (error.code === "P2025") {
-          throw new HTTPError(404, "Data universitas tidak ditemukan.");
+        if (error.code === 'P2025') {
+          throw new HTTPError(404, 'Data universitas tidak ditemukan.');
         }
       }
       throw error;
