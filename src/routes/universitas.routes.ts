1c4f80a516848c606c4b2d74c5d4029b761641f0
FARHANFADHILA <farhanfd.112003@gmail.com>
2026-01-16 17:19:52 +0700

    🔥feat: add route for update universitas

--- src/routes/universitas.routes.ts
+++ src/routes/universitas.routes.ts
@@ -1,7 +1,7 @@
 // src/routes/universitas.route.ts
-import { Hono } from "hono";
-import { container } from "../container";
-import { UniversitasController } from "@controllers/data-master/universitas.controller";
+import { UniversitasController } from '@controllers/data-master/universitas.controller';
+import { Hono } from 'hono';
+import { container } from '../container';
 
 const univRoute = new Hono();
 
@@ -10,8 +10,9 @@ const controller = container.resolve(UniversitasController);
 
 // ✅ SEKARANG INI AKAN BERHASIL
 // Karena controller.getAllUniversitas tipe return-nya sudah 'Response', bukan 'Array' lagi.
-univRoute.get("/", controller.getAllUniversitas);
-univRoute.post("/", controller.createUniversitas);
-univRoute.delete("/:id", controller.deleteUniversitas);
+univRoute.get('/', controller.getAllUniversitas);
+univRoute.post('/', controller.createUniversitas);
+univRoute.delete('/:id', controller.deleteUniversitas);
+univRoute.put('/:id', controller.updateUniversitas);
 
 export default univRoute;
