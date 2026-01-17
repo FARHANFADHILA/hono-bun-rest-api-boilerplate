690c64a9459636911ae812559f4f0fece1a360e3
FARHANFADHILA <farhanfd.112003@gmail.com>
2026-01-17 23:23:03 +0700

    🔥feat: adding faculty route

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,22 @@
-import "reflect-metadata";
+import 'reflect-metadata';
 
-import { Hono } from "hono";
+import { Hono } from 'hono';
 
-import universitasRoute from "@routes/universitas.routes";
-import { loggerMiddleware } from "@middlewares/global/logger";
-import { onErrorHandler } from "@middlewares/global/error-handler";
+import { onErrorHandler } from '@middlewares/global/error-handler';
+import { loggerMiddleware } from '@middlewares/global/logger';
+import fakultasRoute from '@routes/fakultas.routes';
+import universitasRoute from '@routes/universitas.routes';
 const app = new Hono();
 
 app.onError(onErrorHandler);
-app.use("*", loggerMiddleware());
-app.get("/", (c) => {
-  return c.text("Hello Hono ! Server is running 🚀");
+
+app.use('*', loggerMiddleware());
+app.get('/', (c) => {
+  return c.text('Hello Hono ! Server is running 🚀');
 });
 
-app.route("/universitas", universitasRoute);
+app.route('/universitas', universitasRoute);
+
+app.route('/fakultas', fakultasRoute);
+
 export default app;
