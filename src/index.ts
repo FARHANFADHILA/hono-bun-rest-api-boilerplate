11457e1d27b1f8999440bf8d92de23116aa086f9
FARHANFADHILA <farhanfd.112003@gmail.com>
2026-09-11 16:58:38 +0700

    ⚙️refactor: remove unused route imports and related code from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
-import 'reflect-metadata';
-
 import { Hono } from 'hono';
 
 import { onErrorHandler } from '@middlewares/global/error-handler';
 import { loggerMiddleware } from '@middlewares/global/logger';
-import fakultasRoute from '@routes/fakultas.routes';
-import universitasRoute from '@routes/universitas.routes';
 const app = new Hono();
 
 app.onError(onErrorHandler);
@@ -14,9 +10,5 @@ app.use('*', loggerMiddleware());
 app.get('/', (c) => {
   return c.text('Hello Hono ! Server is running 🚀');
 });
-
-app.route('/universitas', universitasRoute);
-
-app.route('/fakultas', fakultasRoute);
 
 export default app;
